b76ed76243919bfab80775e5f713652ff7f4cacb
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2025-01-14 15:44:31 +0500

    added the type page there into the product

--- react/my-app/src/components/TypeSelector/VisualizationCardProps.tsx
+++ react/my-app/src/components/TypeSelector/VisualizationCardProps.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'antd';
-import '../styles/VisualizationCard.css';
+
+import styles from '../styles/VisualizationCard.module.css';
 
 interface VisualizationCardProps {
   name: string;
@@ -12,6 +13,7 @@ const VisualizationCard: React.FC<VisualizationCardProps> = ({ name, image, onCl
   return (
     <Card
       hoverable
+      className={styles.card}
       style={{ width: 300, margin: '10px' }}
       cover={<img alt={name} src={image} />}
       onClick={onClick}
