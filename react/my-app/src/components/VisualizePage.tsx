720dc8868b0c567261f0318eeaeaee794a96ad30
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2025-01-15 12:19:40 +0500

    udpate the shape in the graph

--- react/my-app/src/components/VisualizePage.tsx
+++ react/my-app/src/components/VisualizePage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Spin, Row, Col, Card, Modal } from "antd";
+import { Button, Spin, Row, Col, Card, Modal, Select } from "antd";
 import { useNavigate } from "react-router-dom";
 import { ForceGraph2D } from "react-force-graph";
 import { useSelector } from "react-redux";
@@ -8,7 +8,8 @@ import { RootState } from "../store";
 import Legend from "./Legend";
 import NodeInfoTable from "./NodeInfoTable";
 
-// Define Node and Link types
+const { Option } = Select;
+
 interface Node {
   id: string;
   type: string;
@@ -35,6 +36,8 @@ const VisualizePage: React.FC = () => {
   const [linkColors, setLinkColors] = useState<Record<string, string>>({});
   const [selectedNode, setSelectedNode] = useState<Node | null>(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [entity1Shape, setEntity1Shape] = useState<string>("circle");
+  const [entity2Shape, setEntity2Shape] = useState<string>("square");
 
   const navigate = useNavigate();
   const data = useSelector((state: RootState) => state.data.CopyData);
@@ -43,20 +46,16 @@ const VisualizePage: React.FC = () => {
     if (data) {
       setLoading(true);
       const transformed: GraphData = transformData(data);
-
       const uniqueNodeTypes = Array.from(new Set(transformed.nodes.map((node) => node.type)));
       const uniqueLinkTypes = Array.from(new Set(transformed.links.map((link) => link.type)));
-
       const initialNodeColors: Record<string, string> = {};
       uniqueNodeTypes.forEach((type, index) => {
         initialNodeColors[type] = `hsl(${(index * 60) % 360}, 70%, 50%)`;
       });
-
       const initialLinkColors: Record<string, string> = {};
       uniqueLinkTypes.forEach((type, index) => {
         initialLinkColors[type] = `hsl(${(index * 60 + 180) % 360}, 70%, 50%)`;
       });
-
       setNodeColors(initialNodeColors);
       setLinkColors(initialLinkColors);
       setGraphData(transformed);
@@ -64,32 +63,29 @@ const VisualizePage: React.FC = () => {
     }
   }, [data]);
 
-  const handleColorChange = (type: string, newColor: string, isNode: boolean) => {
-    if (isNode) {
-      setNodeColors({ ...nodeColors, [type]: newColor });
-    } else {
-      setLinkColors({ ...linkColors, [type]: newColor });
-    }
-  };
-
   const drawNode = (node: Node, ctx: CanvasRenderingContext2D) => {
-    const shapeSize = 8;
+    const size = 8;
     const color = nodeColors[node.type] || "#999";
-
     ctx.fillStyle = color;
 
-    if (node.group === "entity_2_type") {
-      ctx.beginPath();
-      ctx.moveTo(node.x!, node.y! - shapeSize);
-      ctx.lineTo(node.x! - shapeSize, node.y! + shapeSize);
-      ctx.lineTo(node.x! + shapeSize, node.y! + shapeSize);
-      ctx.closePath();
-      ctx.fill();
-    } else {
-      ctx.beginPath();
-      ctx.arc(node.x!, node.y!, shapeSize, 0, 2 * Math.PI, false);
-      ctx.fill();
+    ctx.beginPath();
+    const shape = node.group === "entity_1_type" ? entity1Shape : node.group === "entity_2_type" ? entity2Shape : "circle";
+
+    switch (shape) {
+      case "circle":
+        ctx.arc(node.x!, node.y!, size, 0, 2 * Math.PI);
+        break;
+      case "square":
+        ctx.rect(node.x! - size, node.y! - size, size * 2, size * 2);
+        break;
+      case "triangle":
+        ctx.moveTo(node.x!, node.y! - size);
+        ctx.lineTo(node.x! - size, node.y! + size);
+        ctx.lineTo(node.x! + size, node.y! + size);
+        ctx.closePath();
+        break;
     }
+    ctx.fill();
   };
 
   const handleNodeClick = (node: Node) => {
@@ -102,48 +98,45 @@ const VisualizePage: React.FC = () => {
     setSelectedNode(null);
   };
 
-  const handleGoBack = () => {
-    navigate("/upload");
-  };
-
   return (
     <div style={{ padding: "20px" }}>
-      <br />
-      <Button type="default" onClick={handleGoBack}>
-        Go Back to Upload
-      </Button>
+      <Button type="default" onClick={() => navigate("/upload")}>Go Back to Upload</Button>
       <h1>Visualize Data</h1>
+      <Row gutter={16} style={{ marginBottom: 20 }}>
+        <Col>
+          <Select value={entity1Shape} style={{ width: 200 }} onChange={setEntity1Shape}>
+            <Option value="circle">Circle (Entity 1)</Option>
+            <Option value="square">Square (Entity 1)</Option>
+            <Option value="triangle">Triangle (Entity 1)</Option>
+          </Select>
+        </Col>
+        <Col>
+          <Select value={entity2Shape} style={{ width: 200 }} onChange={setEntity2Shape}>
+            <Option value="circle">Circle (Entity 2)</Option>
+            <Option value="square">Square (Entity 2)</Option>
+            <Option value="triangle">Triangle (Entity 2)</Option>
+          </Select>
+        </Col>
+      </Row>
       {loading ? (
         <Spin size="large" />
       ) : (
         <Row gutter={24}>
-          <Col span={6}>
-            <Legend
-              nodeColors={nodeColors}
-              linkColors={linkColors}
-              onColorChange={handleColorChange}
-            />
-          </Col>
+          <Col span={6}><Legend nodeColors={nodeColors} linkColors={linkColors} onColorChange={() => {}} /></Col>
           <Col span={18}>
             <Card title="Graphs" style={{ width: "100%" }}>
-              <div style={{ width: "100%", height: "500px" }}>
-                <ForceGraph2D
-                  graphData={graphData}
-                  nodeLabel="entity"
-                  linkLabel="type"
-                  nodeAutoColorBy="type"
-                  nodeCanvasObject={drawNode}
-                  linkColor={(link: any) => linkColors[link.type] || "#999"}
-                  width={1000}
-                  height={500}
-                  onNodeClick={handleNodeClick}
-                />
-              </div>
+              <ForceGraph2D
+                graphData={graphData}
+                nodeCanvasObject={drawNode}
+                linkColor={(link: any) => linkColors[link.type] || "#999"}
+                width={1000}
+                height={500}
+                onNodeClick={handleNodeClick}
+              />
             </Card>
           </Col>
         </Row>
       )}
-
       <Modal
         title="Node Information"
         open={isModalVisible}
